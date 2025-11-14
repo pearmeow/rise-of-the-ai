29b9d8e0f1db517939212cfcb9725d45cb15ef03
Perry Huang <perryhuang2015@gmail.com>
2025-11-14 15:27:16 -0500

    feat: turn off colliders

--- CS3113/Entity.cpp
+++ CS3113/Entity.cpp
@@ -356,7 +356,7 @@ void Entity::render() {
     // Render the texture on screen
     DrawTexturePro(mTexture, textureArea, destinationArea, originOffset, mAngle, WHITE);
 
-    displayCollider();
+    // displayCollider();
 }
 
 void Entity::displayCollider() {
